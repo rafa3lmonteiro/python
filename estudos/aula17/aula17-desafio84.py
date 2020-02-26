b407f325e54b0f6a92e04c03aa0e72d70b6713fa
Rafael <42952730+rafa3lmonteiro@users.noreply.github.com>
2020-02-26 19:08:10 -0300

    Update aula17-desafio84.py

--- estudos/aula17/aula17-desafio84.py
+++ estudos/aula17/aula17-desafio84.py
@@ -3,3 +3,33 @@
 # B) Uma listagem com as pessoas mais pesadas.
 # C) Uma listagem com as pessoas mais leves.
 
+temporaria = []
+principal = []
+mai = men = 0
+while True:
+    temporaria.append(str(input('Nome: ')))
+    temporaria.append(float(input('Peso: ')))
+    if len(principal) == 0:
+        mai = men = temporaria[1]
+    else:
+        if temporaria[1] > mai:
+            mai = temporaria[1]
+        if temporaria[1] < men:
+            men = temporaria[1]
+    principal.append(temporaria[:])
+    temporaria.clear()
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('=-'*30)
+print(f'Foram cadastradas {len(principal)} Pessoas')
+print(f'O maior peso foi o de {mai}KG. Peso de ', end='')
+for p in principal:
+    if p[1] == mai:
+        print(f'[{p[0]}] ', end='')
+print()
+print(f'O menor peso foi o de {men}KG. Peso de ', end='')
+for p in principal:
+    if p[1] == men:
+        print(f'[{p[0]}] ', end='')
+print()
